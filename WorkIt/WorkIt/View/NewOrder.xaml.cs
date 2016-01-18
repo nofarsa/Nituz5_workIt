47bf03d01bdd20a37dab7610e4bae9fd6d1b22c1
anatzaltzman <anat.zaltz@gmail.com>
2016-01-18 19:19:43 +0200

    Add Classes

--- WorkIt/WorkIt/View/NewOrder.xaml.cs
+++ WorkIt/WorkIt/View/NewOrder.xaml.cs
@@ -19,9 +19,51 @@ namespace WorkIt.View
     /// </summary>
     public partial class NewOrder : Window
     {
-        public NewOrder()
+        Dictionary<string, string> param;
+        Dictionary<string, string> paramitem;
+        public Controller m_controller;
+        public NewOrder(Controller c)
         {
             InitializeComponent();
+            this.m_controller = c;
+            
+        }
+
+        private bool m_cancel = false;
+        public bool Cancel { get { return m_cancel; } set { m_cancel = value; } }
+
+        private void btn_ok_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtbx_OrderID.Text != "")
+            {
+                Cancel = false;
+                param = new Dictionary<string, string>();
+                paramitem = new Dictionary<string, string>();
+                if (txtbx_OrderID.Text != "")
+                    param["OrderID"] = txtbx_OrderID.Text;
+                if (txtbx_Suppplier.Text != "")
+                    param["Suppplier"] = txtbx_Suppplier.Text;
+                if (txtbx_Destination.Text != "")
+                    param["Destination"] = txtbx_Destination.Text;
+                AddItem at = new AddItem();
+                at.ShowDialog();
+                paramitem = at.GetDic();
+                m_controller.CreateOrder(param, paramitem);
+                
+            }
+            else
+            {
+                Cancel = true;
+                MessageBox.Show("Error: Please enter valide fields");
+            }
+            
+
+        }
+
+        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Cancel = true;
+            this.Close();
         }
     }
 }
---
+++ WorkIt/WorkIt/View/SendEvent.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WorkIt.View
+{
+    /// <summary>
+    /// Interaction logic for SendEvent.xaml
+    /// </summary>
+    public partial class SendEvent : Window
+    {
+        //Dictionary<string, string> param;
+        public Controller m_controller;
+
+        public SendEvent(Controller c)
+        {
+            InitializeComponent();
+            this.m_controller = c;
+        }
+
+        private bool m_cancel = false;
+        public bool Cancel { get { return m_cancel; } set { m_cancel = value; } }
+
+        private void btn_ok_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtbx_EventName.Text != "")
+            {
+                m_controller.SendEvent(txtbx_EventName.Text);
+            }
+            this.Close();
+
+        }
+
+        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Cancel = true;
+            this.Close();
+        }
+    }
+}
